a27586d7cb633d176df285ad75767287317bca65
Touhidul Islam <thisis9teen@gmail.com>
2026-08-14 00:09:39 +0600

    Fix addition and subtraction methods in Calculator

--- Calculator.java
+++ Calculator.java
@@ -1,11 +1,11 @@
 public class Calculator {
 
     public int addition(int a, int b) {
-        return a - b;
+        return a + b;
     }
 
     public int subtraction(int a, int b) {
-        return a + b;
+        return a - b;
     }
 
     public int multiplication(int a, int b) {
